2d49dc824147f81f196b82ca2a35d50d3671669b
kimys <kimyslive7@naver.com>
2024-06-21 15:10:04 +0900

    loading
    test
    검토 후

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import styled, {createGlobalStyle} from 'styled-components';
 import reset from 'styled-reset';
+import Loading from './loadingItem/Loading';
 
   const GlobalStyles = createGlobalStyle`
   ${reset}
 `;
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); 
+  }, []);
+
   return (
     <>
       <GlobalStyles />
+
+      <div className="App">
+      {isLoading ? <Loading/> : <h1>메인페이지</h1>}
+    </div>
     </>
   );
 }
---
+++ src/loadingItem/Loading.jsx
@@ -0,0 +1,17 @@
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="footprint left"></div>
+      <div className="footprint right"></div>
+      <div className="footprint up"></div>
+      <div className="footprint down"></div>
+      <div className="footprint left"></div>
+      <div className="footprint right"></div>
+      <div className="footprint up"></div>
+    </div>
+  );
+};
+
+export default Loading;
