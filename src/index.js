011112b99ef996a84d051557a35fa4a41e849f00
dahun <fkdlwldj02@naver.com>
2024-06-25 16:52:56 +0900

    로그인 페이지

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './loginPage/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App />
+    // <App />
+    <Login />
 );
 
 // If you want to start measuring performance in your app, pass a function
