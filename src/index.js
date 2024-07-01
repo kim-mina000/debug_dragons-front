98d5cb5cdc43281bbd012894fc11aafb4e385197
dahun <fkdlwldj02@naver.com>
2024-07-01 09:20:25 +0900

    머지를 위한 푸쉬

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import SignUpComplete from './components/loginPage/SignUpComplete';
-=======
 import { Provider } from 'react-redux';
 import { store } from './store/store';
->>>>>>> 60e02946965a5ad2d3c7e3ee7951a1653fe9b217
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-    <App />
-=======
     <Provider store={store}>
         <App />  
     </Provider>
->>>>>>> 60e02946965a5ad2d3c7e3ee7951a1653fe9b217
 );
 
 // If you want to start measuring performance in your app, pass a function
