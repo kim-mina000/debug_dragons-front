4c8ad568ed8f4103b93264cd0478768c6b92af6c
JEONwhail <green_jeon247@naver.com>
2024-07-04 14:21:00 +0900

    스크랩 페이지 생성 및 스크랩페이지 작업 시작

--- src/components/0.menuBar/MenuBar.jsx
+++ src/components/0.menuBar/MenuBar.jsx
@@ -78,7 +78,9 @@ function MenuBar() {
         </Icon>
       </Link>
 
+      <Link to="/scrap">
       <Icon isVisible={isExpanded}><img src='/스크랩.png' alt="스크랩"/></Icon>
+      </Link>
       
       <Link to="/mypage">
         <Icon isVisible={isExpanded}><img src='/마이페이지.png' alt="마이페이지"/></Icon>
