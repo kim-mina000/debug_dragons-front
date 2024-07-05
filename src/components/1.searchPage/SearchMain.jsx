eac5f25b63daa92fc318fa88c9bb83657a947bc9
mina kim <mina14712@gmail.com>
2024-07-05 13:37:11 +0900

    내일정에저장하기페이지구현

--- src/components/1.searchPage/SearchMain.jsx
+++ src/components/1.searchPage/SearchMain.jsx
@@ -187,25 +187,7 @@ function SearchMain() {
         infowindow.open(map, marker);
       });
     }
-    // var content = '<div class="wrap">' + 
-    // '    <div class="info">' + 
-    // '        <div class="title">' + 
-    // '            카카오 스페이스닷원' + 
-    // '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' + 
-    // '        </div>' + 
-    // '        <div class="body">' + 
-    // '            <div class="img">' +
-    // '                <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">' +
-    // '           </div>' + 
-    // '            <div class="desc">' + 
-    // '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' + 
-    // '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' + 
-    // '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' + 
-    // '            </div>' + 
-    // '        </div>' + 
-    // '    </div>' +    
-    // '</div>';
-    // 이거 이런식으로 등록하래요 ,,
+
 
 // 마커추가
       // 지도를 클릭했을때 클릭한 위치에 마커를 추가하도록 지도에 클릭이벤트를 등록
@@ -233,7 +215,7 @@ function SearchMain() {
         const marker = new kakao.maps.Marker({
           map: map,
           position: position,
-          image: markerImage // 마커 이미지 
+          image: markerImage // 마커 이미지   
         });
 
         // 마커가 지도 위에 표시되도록 설정
