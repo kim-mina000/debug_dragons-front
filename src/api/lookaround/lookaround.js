9115be0709950f8e57e8da100f9924e378b06985
kimys <kimyslive7@naver.com>
2024-07-17 14:07:57 +0900

    landmarkorigin2
    컨트롤단
    landmarkOrigin=2 인 값 가져오기 값넘어오기화긴

--- src/api/lookaround/lookaround.js
+++ src/api/lookaround/lookaround.js
@@ -1,12 +1,13 @@
 import axios from "axios"
 import { BACK_URL } from "../config"
 
+
+// 공유랜드마크 2번 보여줭
 export const getShareTravelList = async () => {
 
   try {
-    const response = await axios.get(`${BACK_URL}//`)
-    console.log(response);
-    return response;
+    const response = await axios.get(`${BACK_URL}/landmark/lookaround`)
+    return response.data;
   } catch (error) {
     console.error(error);
   }
